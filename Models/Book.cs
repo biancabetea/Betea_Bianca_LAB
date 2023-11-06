c6868ac31ba7cdc1434a2d0826c133ddc228fdfd
Betea Bianca Mariana <bianca.betea@stud.ubbcluj.ro>
2023-11-06 20:16:06 +0200

    book

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,6 +2,9 @@
 {
     public class Book
     {
-        
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
     }
 }
