54c3b1c496931689c92bff148f3277a398bf2f51
Betea Bianca Mariana <bianca.betea@stud.ubbcluj.ro>
2023-11-10 12:29:05 +0200

    new

--- Models/Book.cs
+++ Models/Book.cs
@@ -25,6 +25,8 @@ namespace Betea_Bianca_LAB2.Models
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
 
+        public ICollection<BookCategory>? BookCategories { get; set; }
+
     }
 }
 
