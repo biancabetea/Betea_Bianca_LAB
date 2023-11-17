3f07b2be14bfa6358b79416efb9afa1c44d08179
Betea Bianca Mariana <bianca.betea@stud.ubbcluj.ro>
2023-11-17 18:31:37 +0200

    lab5 pana la tema

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Betea_Bianca_LAB2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Betea_Bianca_LAB2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Betea_Bianca_LAB2Context") ?? throw new InvalidOperationException("Connection string 'Betea_Bianca_LAB2Context' not found.")));
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Betea_Bianca_LAB2Context") ?? throw new InvalidOperationException("Connectionstring 'Betea_Bianca_LAB2Context' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
@@ -22,6 +32,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
